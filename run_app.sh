265f88c1f82d8a9a76f0c7bb2d79d88bb340a506
Josias <josias.boelzle@outlook.de>
2024-09-09 20:28:38 +0200

    Removed proxy and added .NET and NodeJS install to bash

--- run_app.sh
+++ run_app.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 
+function install_dotnet {
+    echo ".NET SDK nicht gefunden. Installiere .NET SDK..."
+    wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+    chmod +x dotnet-install.sh
+    ./dotnet-install.sh --channel LTS 
+    export PATH=$PATH:$HOME/.dotnet:$HOME/.dotnet/tools
+    echo ".NET SDK wurde installiert."
+}
+
+function install_node {
+    echo "Node.js nicht gefunden. Installiere Node.js..."
+    curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -  
+    sudo apt-get install -y nodejs
+    echo "Node.js und npm wurden installiert."
+}
+
+if ! command -v dotnet &> /dev/null
+then
+    install_dotnet
+else
+    echo ".NET SDK ist bereits installiert."
+fi
+
+if ! command -v npm &> /dev/null
+then
+    install_node
+else
+    echo "npm (Node.js) ist bereits installiert."
+fi
 
 API_DIR="./layer2Aufgabe.Server"
 
-
 REACT_DIR="./layer2aufgabe.client"
 
-# Installiere Abhängigkeiten für das ASP.NET Core Web API
 echo "Installiere Abhängigkeiten für ASP.NET Core Web API..."
 cd "$API_DIR"
-dotnet restore  
+dotnet restore 
 
-# Start ASP.NET Core Web API
 echo "Starte ASP.NET Core Web API..."
 dotnet run &  
 API_PID=$!    
@@ -20,9 +46,8 @@ echo "Installiere Abhängigkeiten für React App..."
 cd "../$REACT_DIR"
 npm install  
 
-
 echo "Starte React App mit Vite..."
-npm run dev & 
+npm run dev &  
 REACT_PID=$!  
 
 
